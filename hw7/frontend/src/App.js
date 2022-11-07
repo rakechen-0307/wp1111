0a0d40443e2dfac59d2eaba3edd5f6e3dc343776
rakec <rakechen168@gmail.com>
2022-11-07 22:47:34 +0800

    temp

---
+++ hw7/frontend/src/App.js
@@ -0,0 +1,67 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import { Button, Input, message, Tag } from 'antd'
+import useChat from './useChat'
+ 
+function App() {
+  const { status, messages, sendMessage } = useChat()
+  const [username, setUsername] = useState('')
+  const [body, setBody] = useState('')
+
+  const displayStatus = (s) => {
+    if (s.msg) {
+      const { type, msg } = s;
+      const content = {
+        content: msg, duration: 0.5 }
+      switch (type) {
+        case 'success':
+          message.success(content)
+          break
+        case 'error':
+        default:
+          message.error(content)
+          break
+      }
+    }
+  }
+
+  useEffect(() => {
+    displayStatus(status)}, [status])
+
+  return (
+    <div className="App">
+      <div className="App-title">
+        <h1>Simple Chat</h1>
+        <Button type="primary" danger >
+          Clear
+        </Button>
+      </div>
+      <div className="App-messages">
+        {messages.length === 0 ? (
+          <p style={{ color: '#ccc' }}> No messages... </p>
+        ) : (
+          messages.map(({ name, body }, i) => (
+            <p className="App-message" key={i}><Tag color="blue">{name}</Tag> {body}</p>
+          ))
+        )}
+      </div>
+      <Input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        style={{ marginBottom: 10 }}
+      ></Input>
+      <Input.Search
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        enterButton="Send"
+        placeholder="Type a message here..."
+        onSearch={(msg) => {
+          sendMessage({ name: username, body: msg })
+        }}
+      ></Input.Search>
+    </div>
+  )
+}
+
+export default App
