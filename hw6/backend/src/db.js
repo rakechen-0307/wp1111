014dc3734f1bf14cc9e3c5b18c50b6088a7cc3c2
rakec <rakechen168@gmail.com>
2022-11-07 17:25:27 +0800

    final ver.

--- hw6/backend/src/db.js
+++ hw6/backend/src/db.js
@@ -6,7 +6,7 @@ export default {
          /* code to connect Mongoose DB */ 
          dotenv.config({path:__dirname+'/.env'})
          mongoose
-            .connect(process.env.MONGO_URL, {
+            .connect("mongodb+srv://rakechen0307:92chen0307R@wp1111-hw6.k6z7ocx.mongodb.net/?retryWrites=true&w=majority", {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
             })
