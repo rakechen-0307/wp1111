03988c6963768d7248ac6db2d73d7bba6017ebea
rakechen-0307 <rakechen168@gmail.com>
2022-12-20 11:12:08 +0800

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -1,5 +1,4 @@
 import { useQuery } from '@apollo/client';
-import { useState } from 'react';
 
 import { GET_ITEMS_QUERY } from '../graphql/queries';
 
@@ -9,16 +8,18 @@ import Category from './Category';
 function Analytics() {
   // TODO 2.2 Use the useQuery hook to get items from backend
 
+  const { data: items, error, loading } = useQuery(GET_ITEMS_QUERY)
+
   // TODO 2.2 End
 
   return (
     <div className="grid grid-cols-12 gap-6"> 
-      {/*<div className="col-span-6">
+      <div className="col-span-6">
         <Balance items={items} />
       </div>
       <div className="col-span-6">
         <Category items={items} />
-      </div>*/} 
+      </div>
     </div>
   );
 }
